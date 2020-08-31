26b7cf2574469347e66a29c3337cecfbc2928ae7
Joy <JoyMart@users.noreply.github.com>
2020-08-30 21:38:04 -0500

    Create PyAssignment3.py
    creating classes with inheritance

---
+++ python_assignments/PyAssignment3.py
@@ -0,0 +1,21 @@
+
+
+
+#create two classes that inherit from another class.
+#Each child should have at least two of their own attributes.
+
+class clothing_article:
+        material = ""
+        size = "medium"
+        occation = "casual"
+        color = ""
+
+class shirt(clothing_article):
+        sleave_type = "short"
+        pockets = "none"
+
+
+class skirt(clothing_article):
+        length = "18 inches"
+        closure = "buttons"
+                   
