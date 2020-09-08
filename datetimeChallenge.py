2febffe1c1318ca7805a500c91f9076a03c89384
Joy <JoyMart@users.noreply.github.com>
2020-09-08 18:55:08 -0500

    Create datetimechallenge
    find times in different timezones
    compare times to open and close time

--- datetimeChallenge.py
+++ datetimeChallenge.py
@@ -2,14 +2,49 @@
 import datetime
 from pytz import timezone
 import pytz
+import time
 
 
 
 utc = pytz.utc
 utc.zone
+fmt = "%H:%M"
 
-PortlandHQ = timezone('US/Portland')
 
-NYCbranch = timezone('US/Eastern')
+Open = datetime.time(9, 0, 0).strftime(fmt)
+Close = datetime.time(17, 0, 0).strftime(fmt)
 
-LondonBranch = timezone('Britain/London')
+
+PortlandHQ = datetime.datetime.now(pytz.timezone('US/Pacific'))
+print(PortlandHQ.strftime(fmt))
+
+NYCbranch = datetime.datetime.now(pytz.timezone('US/Eastern'))
+print(NYCbranch.strftime(fmt))
+
+LondonBranch = datetime.datetime.now(pytz.timezone('Europe/London'))
+print(LondonBranch.strftime(fmt))
+
+#time in Portland
+timeHQ= PortlandHQ.strftime(fmt)
+if timeHQ  > Open and timeHQ < Close:
+        print("The Portland branch is open!\n")
+else:
+        print("I am sorry, but the Portland branch is currently closed. \nPlease check back at another time!\n")
+        
+
+
+#time in NYC
+timeNYC= NYCbranch.strftime(fmt)
+if  timeNYC > Open and timeNYC < Close:
+        print("The NYC branch is open!\n")
+else:
+        print("I am sorry, but the NYC branch is currently closed. \nPlease check back at another time!\n")
+
+
+
+#time in Portland
+timeLON=LondonBranch.strftime(fmt)
+if  timeLON  > Open and timeLON < Close:
+        print("The London branch is open!\n")
+else:
+        print("I am sorry, but the London branch is currently closed. \nPlease check back at another time!\n")
