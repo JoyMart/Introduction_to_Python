53ce51d5880c254db5dd23b000aa544a97af2c93
Joy <JoyMart@users.noreply.github.com>
2020-09-14 18:06:40 -0500

    create apps/edit hw
    create hello_world app
    create hotel app
    edit sql hw from page 171

--- python_assignments/PyAssignment2.py
+++ python_assignments/PyAssignment2.py
@@ -7,42 +7,29 @@ conn = sqlite3.connect('assign2.db')
 
 with conn:
         cur = conn.cursor()
-        cur.execute('CREATE TABLE IF NOT EXISTS tbl_textFiles( \
+        cur.execute('CREATE TABLE IF NOT EXISTS tbl_txtFiles( \
                col_id INTEGER PRIMARY KEY AUTOINCREMENT, \
-               col_string TEXT \
+               col_files TEXT \
               )' )
         conn.commit
 conn.close()
 
-
-dir1 = r"C:\\Users\\joybe\\Python\\python_assignments\\"
+fileList = ('information.docx', 'Hello.txt', 'myImage.png', \
+            'myMovie.mpg', 'World.txt', 'data.pdf', 'myPhoto.jpg')
                         
-file_list = []
-for filename in os.listdir(dir1):
-        if filename.endswith('.txt'):
-                file_list.append(filename)
-                print(os.path.join(dir1, filename))
-        else:
-                continue
-print(file_list)
 
 
 conn = sqlite3.connect('assign2.db')
 with conn:
         cur = conn.cursor()
-        cur.execute("INSERT INTO tbl_textFiles (col_string) VALUES( ?)", \
-                    (file_list[0], ))
-        cur.execute("INSERT INTO tbl_textFiles (col_string) VALUES( ?)", \
-                    (file_list[1], ))
-        cur.execute("INSERT INTO tbl_textFiles (col_string) VALUES( ?)", \
-                    (file_list[2], ))
+        for filename in fileList:
+                if filename.endswith('.txt'):
+                        print(filename)
+                        cur.execute("INSERT INTO tbl_txtFiles (col_files) VALUES( ?)", (filename,))
+                else:
+                        continue
         conn.commit()
 conn.close()
-
-
-
-
-
 
 
 
